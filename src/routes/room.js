a43af5b329df5f6ff6dd5af93da75b99943206b8
성현주 <vivivim@github.com>
2024-12-10 18:23:39 +0900

    Feat: join 로직

--- src/routes/room.js
+++ src/routes/room.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
 const Room = require('../../models/Room');
+const Participant = require('../../models/Participant');
 const bcrypt = require('bcrypt');
 
 router.post('/create', authMiddleware, async (req, res) => {
-
   try {
     const { title, thumbnail, description, isPrivate, password, maxKnitter } = req.body;
 
@@ -29,19 +29,45 @@ router.post('/create', authMiddleware, async (req, res) => {
       message: 'Room created successfully.',
     });
   } catch (error) {
-    console.error(error.message);
+    console.error('Room create error: ' + error.message);
     res.status(400).json({ message: 'Failed to create room.' });
   }
 });
 
 router.get('/list', authMiddleware, async (req, res) => {
-    try {
-        const rooms = await Room.find({}, 'title thumbnail description isPrivate maxKnitter');
-        res.status(200).json({ rooms });
-    } catch (error) {
-        console.error(error.message);
-        res.status(400).json({ message: 'Failed to load room list' });
-    }
+  try {
+      const rooms = await Room.find({}, 'title thumbnail description isPrivate maxKnitter');
+      res.status(200).json({ rooms });
+  } catch (error) {
+      console.error('Room list error: ' + error.message);
+      res.status(400).json({ message: 'Failed to load room list' });
+  }
+});
+
+router.post('/join', authMiddleware, async (req, res) => {
+  try {
+    const { userId, roomId } = req.body;
+    const room = await Room.findById(roomId);
+    
+    // if (!room) {
+    //   return res.status(404).json({ message: 'Room not found' });
+    // }
+
+    const participant = await Participant.create({
+      userId,
+      roomId,
+      role: 'member',
+    });
+    await participant.save();
+    
+    res.status(200).json({
+      message: 'Joined room successfully',
+      participant,
+    });
+  } catch (error) {
+    console.error('Room join error:' + error.message);
+    res.status(500).json({ message: 'Failed to join room' });
+  }
 });
 
 module.exports = router;
